82a2d5d54717e7ba49f536425b4b11d37d95f9bb
litvinenko.b <litvinenko.b@mjr.ru>
2025-09-30 16:34:57 +0300

    Initial 2.1

---
+++ Domain/ValueObjects/Location.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObjects;
+
+public record Location
+{
+    public string Country { get; } = default!;
+    public string Region { get; } = default!;
+    public string City { get; } = default!;
+    public string State { get; } = default!;
+    public string HouseNumber { get; } = default!;
+    public string Index { get; } = default!;
+
+    private Location(string country, string region, string city, string state, string houseNumber, string index)
+    {
+        Country = country;
+        Region = region;
+        City = city;
+        State = state;
+        HouseNumber = houseNumber;
+        Index = index;
+    }
+
+    public static Location Of(string country, string region, string city, string state, string houseNumber, string index)
+    {
+        //Проверка на отсутствие значения
+        ArgumentException.ThrowIfNullOrWhiteSpace(country);
+        ArgumentException.ThrowIfNullOrWhiteSpace(region);
+        ArgumentException.ThrowIfNullOrWhiteSpace(city);
+        ArgumentException.ThrowIfNullOrWhiteSpace(state);
+        ArgumentException.ThrowIfNullOrWhiteSpace(houseNumber);
+        ArgumentException.ThrowIfNullOrWhiteSpace(index);
+
+        return new Location(country, region, city, state, houseNumber, index);
+    }
+
+}
